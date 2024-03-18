6aa93eaad0c9c3091d50de84f2238e3aececa28b
“mavine4512” <mavineoyugi@gmail.com>
2024-03-18 00:13:25 -0700

    fixed error

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,5 +8,5 @@ def minOperations(n):
         return 0
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
-            return i + minOperation(n // i)
+            return i + minOperations(n // i)
     return n
