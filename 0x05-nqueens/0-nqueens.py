bfcbd3bae8d93fa2cc873a9d3954289d4c380154
“mavine4512”  <mavineoyugi@gmail.com>
2024-04-11 13:49:56 -0700

    fixed error

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,7 +4,7 @@
 import sys
 
 
-solution = []
+solutions = []
 """The list of posible solutions to the N queens problem.
 """
 n = 0
@@ -47,7 +47,7 @@ def is_attacking(pos0, pos1):
     Return:
         bool: True if the queen are in an attacking position else False.
     """
-    if (pos0[0] == pos[1]) or (pos0[1] == pos1[1]):
+    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
         return True
     return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
 
@@ -64,7 +64,7 @@ def group_exists(group):
     global solutions
     for stn in solutions:
         i = 0
-        for stn_pos in stn:
+       for stn_pos in stn:
            for grp_pos in group:
                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
                    i += 1
@@ -92,7 +92,7 @@ def build_solution(row, group):
             matches = zip(list([pos[a]]) * len(group), group)
             used_position = map(lambda x: is_attacking(x[0], x[1]), matches)
             group.append(pos[a].copy())
-            if not any(used_position):
+            if not any(used_positions):
                 build_solution(row + 1, group)
             group.pop(len(group) - 1)
 
